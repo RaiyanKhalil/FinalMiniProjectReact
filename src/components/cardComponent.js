f63cc17acfb6e241173495966e6d3027d908f6e9
RaiyanKhalil <raiyankhalil@gmail.com>
2022-11-24 13:12:01 -0800

    navbar

--- src/components/cardComponent.js
+++ src/components/cardComponent.js
@@ -3,13 +3,6 @@ import '../App.css';
 const CardComponent = () => {
 
     return(
-        // <div className="cardBody">
-        //     <img className="img-css" src="https://randomuser.me/api/portraits/men/75.jpg" alt="place-holder-image" />
-        //     <p>Name: Jennie Nicolas</p>
-        //     <p>Email: johndoe@example.com</p>
-        //     <p>Phone: 2222222222</p>
-        //     <p>Address: V0M 4O9</p>
-        // </div>
         <div class="column">
             <div class="card">
             <img className="img-css" src="https://randomuser.me/api/portraits/men/75.jpg" alt="place-holder-image" />
