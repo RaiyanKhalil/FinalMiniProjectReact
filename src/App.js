94707d5757445f6e2d9bf571095ed648a6a7012f
RaiyanKhalil <raiyankhalil@gmail.com>
2022-11-28 19:20:33 -0800

    add cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 import CardComponent from './components/cardComponent'
 
 
+function getList() {
+  return fetch('http://universities.hipolabs.com/search?country=canada')
+    .then(data => data.json())
+}
+
+
+// function hideCard(){
+//   let carsNum = document.querySelectorAll(".column")
+
+//   // carsNum.
+// }
+
 function App() {
 
-  const rows = [];
 
-  for (let index = 0; index < 12; index++) {
-    // const element = array[index];
-    rows.push(<CardComponent key={index} />)
-  }
+
+  const [list, setList] = useState([]);
+  const [cardNum, setCardNum] = useState(12)
   
+  useEffect(() => {
+    let mounted = true;
+    getList()
+      .then(items => {
+        if(mounted) {
+          // console.log(items)
+          setList(items)
+        }
+      })
+    return () => mounted = false;
+  }, [])
+  
+  // hideElem()
+
+
+ 
 
   return (
     <div class="row">
       <h2>Responsive Column Cards</h2>
       <p>Resize the browser window to see the effect.</p>
+      <button onClick={() => setCardNum(cardNum + 1)}>Add card</button>
+      <button onClick={() => setCardNum(cardNum - 1)}>Remove card</button>
 
-        {rows}
+            <h1>My Canada university list</h1>
+
+
+          {
+          list.slice(0 ,  cardNum).map((elem, index) => {
+                return(
+                  <CardComponent key={index} data={elem} />
+                );
+
+            }
+          )}
     </div>
   );
 }
--- src/components/cardComponent.js
+++ src/components/cardComponent.js
@@ -1,13 +1,13 @@
 import '../App.css';
 
-const CardComponent = () => {
-
+const CardComponent = (props) => {
+    // console.log(style)
     return(
-        <div class="column">
+        <div class="column" >
             <div class="card">
             <img className="img-css" src="https://randomuser.me/api/portraits/men/75.jpg" alt="place-holder-image" />
-            <h3>Card 4</h3>
-            <p>Some text</p>
+            <h3>{props.data.name}</h3>
+            <a href={props.data.domains[0]}>{props.data.domains[0]}</a>
             <p>Some text</p>
             </div>
         </div>
