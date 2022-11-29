af7f657d8b23046fd2f3851f21b60e52a1722d74
niniwoo <qhdk6914@naver.com>
2022-11-29 13:50:49 -0800

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import CardComponent from './components/cardComponent'
+import JeonginPractice from './components/JeonginPractice';
 
 
 function App() {
 
   const rows = [];
+
 
   for (let index = 0; index < 12; index++) {
     // const element = array[index];
@@ -16,10 +18,12 @@ function App() {
   return (
     <div class="row">
       <h2>Responsive Column Cards</h2>
+      <JeonginPractice/>
       <p>Resize the browser window to see the effect.</p>
 
         {rows}
     </div>
+    
   );
 }
 
---
+++ src/components/jeonginPractice.js
@@ -0,0 +1,12 @@
+// practice components by Jeongin 
+import React from 'react';
+import '../App.css';
+const JeonginPractice = () => {
+//create modal
+    return(
+       <div>This is jeongin </div>
+    )
+}
+
+
+export default JeonginPractice
